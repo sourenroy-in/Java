23089276621d18a1befecc781d1220e24376a6e1
souren roy <sourenroy.in@gmail.com>
2026-08-24 19:48:31 +0530

    Problem2

---
+++ src/Problem_2.java
@@ -0,0 +1,8 @@
+public class Problem_2 {
+    public static void main(String[] args) {
+        int a = 10, b = 2, c;
+        c = a * b;
+
+        System.out.println("Product is : " + c);
+    }
+}
