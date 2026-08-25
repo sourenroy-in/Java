605a26475f8ea933cc14b1c4e9639685fdcbe5e4
souren roy <sourenroy.in@gmail.com>
2026-08-25 22:10:51 +0530

    Problem3

---
+++ src/Problem_3.java
@@ -0,0 +1,10 @@
+public class Problem_3 {
+        public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        int c = a+b;
+        System.out.println("The Sum is: "+c);
+        c = a * b;
+        System.out.println("Product is : " + c);
+    }
+}
