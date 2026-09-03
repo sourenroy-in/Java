336151036cf8b44b47a700d47dca322ec8e1af15
souren roy <sourenroy.in@gmail.com>
2026-09-03 10:16:17 +0530

    09

---
+++ src/Problem_9.java
@@ -0,0 +1,24 @@
+public class Problem_9 {
+    public static void main(String args[]){
+        int n = 10;
+        while(n > 0){
+            System.out.println(n);
+            n--;
+        }
+    }
+}
+
+/* 
+
+public class Problem_9 {
+    public static void main(String[] args) {
+        int n = 1;
+
+        while (n <= 10) {
+            System.out.println(n);
+            n++;
+        }
+    }
+}
+
+*/
