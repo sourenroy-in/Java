f5d0dcecdb654e2ac206da6aba81a3080a8e446f
souren roy <sourenroy.in@gmail.com>
2026-09-05 01:19:39 +0530

    12

---
+++ src/Problem_12.java
@@ -0,0 +1,18 @@
+public class Problem_12 {
+    public static void main(String[] args) {
+        int n = 1;
+
+        switch (n) {
+            case 1:
+                System.out.println("Case1");
+                break;
+            case 2:
+                System.out.println("Case2");
+                break;
+                
+                default:
+                System.out.println("Nothing");
+                break;
+        }
+    }
+}
