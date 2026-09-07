84644ef233c5e7c65d7f0ad0f19bc73ded4d042e
souren roy <sourenroy.in@gmail.com>
2026-09-07 20:19:57 +0530

    14

---
+++ src/Problem_14.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Problem_14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+
+        scanner.close();
+    }
+    
+}
