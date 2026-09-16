6c1c64f86e7a430205501f61cf38985b9495d98c
souren roy <sourenroy.in@gmail.com>
2026-09-16 17:58:13 +0530

    26

---
+++ src/Problem26.java
@@ -0,0 +1,20 @@
+// Find Even/Odd 
+
+import java.util.Scanner;;
+
+public class Problem26 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        if(n % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        sc.close();
+    }
+    
+}
