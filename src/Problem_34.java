49a6efba40be23e9652e15b2b1bbe2358289c002
souren roy <sourenroy.in@gmail.com>
2026-09-24 21:01:02 +0530

    34

---
+++ src/Problem_34.java
@@ -0,0 +1,25 @@
+
+//
+import java.util.Scanner;
+
+public class Problem_34 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of Array :");
+        int n = sc.nextInt();
+
+        System.out.println("Enter elements of Array: ");
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}
