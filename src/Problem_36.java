3535c96a21789ba50a53cf39aa1f41aba144fc3c
souren roy <sourenroy.in@gmail.com>
2026-09-25 22:24:17 +0530

    36

---
+++ src/Problem_36.java
@@ -0,0 +1,12 @@
+public class Problem_36 {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        int sum = a + b + c;
+
+        System.out.println(sum);
+    }
+
+}
