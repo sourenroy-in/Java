f658dda6ac6c4a4e824a2195a98564d7aeaa4c10
souren roy <sourenroy.in@gmail.com>
2026-09-25 22:30:05 +0530

    1

--- src/Problem_1.java
+++ src/Problem_1.java
@@ -1,9 +1,9 @@
 // Problem1 Write a program for SUM
 
-public class Problem_1{
+public class Problem_1 {
     public static void main(String[] args) {
         int a = 10, b = 20;
-        int c = a+b;
-        System.out.println("The Sum is: "+c);
+        int c = a + b;
+        System.out.println("The Sum is: " + c);
     }
 }
